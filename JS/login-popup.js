ec202cca13666854900085461d2e2cf3bdee531a
OlgaLihter <116577727+OlgaLihter@users.noreply.github.com>
2022-11-01 05:33:00 +0500

    Add files via upload

--- JS/login-popup.js
+++ JS/login-popup.js
@@ -1,5 +1,6 @@
 let loginButton = document.querySelector(".login-button");
 let loginPopup = document.querySelector(".login-popup");
+let Page = document.querySelector(".page");
 
 loginButton.addEventListener('click', showPopup)
 
@@ -7,5 +8,5 @@ function showPopup(evt){
     console.log(evt);
   evt.preventDefault();
   loginPopup.classList.toggle('show-popup');
-  
+  Page('popup-backdrop')
 }
